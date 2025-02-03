7b00d2d8ccd5161d0209bff5c5412f9a27017e3e
erdem.konak <erdem.konak@VS007071>
2025-02-03 11:31:29 +0300

    Add project files.

---
+++ TravelTripProje/Models/Siniflar/BlogYorum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class BlogYorum
+    {
+
+        public IEnumerable<Blog> Deger1 { get; set; }
+
+        public IEnumerable<Yorum> Deger2 { get; set; }
+
+        public IEnumerable<Blog> Deger3 { get; set; }
+
+
+
+
+    }
+}
